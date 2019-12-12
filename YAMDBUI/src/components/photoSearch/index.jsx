6de255b8029ccccb7d587d3eb2bf4fe12a197f0a
ZachMayer <GenericGuy35@gmail.com>
2019-12-12 01:09:28 -0800

    Update index.jsx

--- YAMDBUI/src/components/photoSearch/index.jsx
+++ YAMDBUI/src/components/photoSearch/index.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { push } from 'connected-react-router';
 import Camera from 'react-html5-camera-photo';
-import axios from 'axios';
 import 'react-html5-camera-photo/build/css/index.css';
 
 
 const PhotoSearch = ({ handleSearch }) => {
-  const dispatch = useDispatch();
   return (<Camera isImageMirror={false} style={{width: '100%', height: '100%', position: 'absolute'}} 
     onTakePhoto = { (image) => { handleSearch(image); } }
   />);
